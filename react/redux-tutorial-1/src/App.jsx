cd4993388dc62f4b1e5cd2db68a1b1d0ac8cc542
Sultepronit <sultepronit@gmail.com>
2024-08-06 15:25:10 +0300

    started following redux tutorial

---
+++ react/redux-tutorial-1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  console.log('here we go!');
+
+  return (
+    <>
+      <h1>Here we go!</h1>
+    </>
+  )
+}
+
+export default App
