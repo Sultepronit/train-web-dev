b180302c924b84736d1851ab327745321cff4575
Sultepronit <sultepronit@gmail.com>
2024-08-08 21:08:56 +0300

    started learning RTK Query

---
+++ react/redux-rtk-tutorial-1/src/features/todos/TodoList.jsx
@@ -0,0 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+export default function TodoList() {
+    return <h1>Here we go!</h1>
+}
