f5f921ff7edf7b5716e776803569d2ff29c652d9
Sultepronit <sultepronit@gmail.com>
2024-08-09 13:19:21 +0300

    finished the redux/rtk tutorial

--- react/redux-rtk-tutorial-1/src/features/todos/TodoList.jsx
+++ react/redux-rtk-tutorial-1/src/features/todos/TodoList.jsx
@@ -1,7 +1,79 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import {
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation
+ } from "../api/apiSlice";
 
 export default function TodoList() {
-    return <h1>Here we go!</h1>
+    const [newTodo, setNewTodo] = useState('');
+
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery();
+
+    const [addTodo] = useAddTodoMutation();
+    const [updateTodo] = useUpdateTodoMutation();
+    const [deleteTodo] = useDeleteTodoMutation();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        addTodo({ userId: 1, title: newTodo, completed: false });
+        setNewTodo('');
+    }
+
+    const newItemSection = 
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="new-todo">Enter a new todo item</label>
+            <div className="new-todo">
+                <input
+                    type="text"
+                    id="new-todo"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder="Enter new todo"
+                />
+            </div>
+            <button className="submit">
+                <FontAwesomeIcon icon={faUpload} />
+            </button>
+        </form>
+
+        const list = todos?.map(todo => {
+            return (
+                <article key={todo.id}>
+                    <div className="todo">
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            id={todo.id}
+                            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </article>
+            );
+        })
+
+        const content = isLoading ? <p>Loading...</p>
+            : isSuccess ? list
+                : isError ? <p>{error.error}</p> : '';
+
+        return (
+            <main>
+                <h1>Todo List</h1>
+                {newItemSection}
+                {content}
+            </main>
+        );
 }
--- react/redux-rtk-tutorial-1/src/main.jsx
+++ react/redux-rtk-tutorial-1/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { apiSlice } from './features/api/apiSlice.js';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </StrictMode>,
 )
